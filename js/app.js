b134e08f77d1eecfa24e8073daebaa6061e98693
LCap <lcap705@.gmail.com>
2022-08-14 17:34:46 +0300

    qqqqqqq

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const animItems = document.querySelectorAll(".__anim-items");
+
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll() {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("__active");
+      } else {
+        if (!animItem.classList.contains("__anim-no-hide")) {
+          animItem.classList.remove("__active");
+        }
+      }
+    }
+  }
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+
+  setTimeout(() => {
+    animOnScroll();
+  }, 300);
+}
